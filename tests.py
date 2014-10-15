3c806af85dcab03c1fa96a6b5a714577dbf60387
Belousow Makc <lib.bmw@gmail.com>
2014-10-16 01:06:21 +0400

    Add tests for subtraction

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class IntegerTest(unittest.TestCase):
     def setUp(self):
         Integer.prime = 9
 
-    def test_addition(self):
+    def test_add(self):
         a = Integer(7)
         b = Integer(2)
         self.assertEqual((a + b).value, 0)
@@ -18,7 +18,7 @@ class IntegerTest(unittest.TestCase):
         self.assertEqual((3 + b).value, 5)
         self.assertEqual((8 + b).value, 1)
 
-    def test_multiplication(self):
+    def test_mul(self):
         a = Integer(7)
         b = Integer(2)
         self.assertEqual((a * b).value, 5)
@@ -28,6 +28,21 @@ class IntegerTest(unittest.TestCase):
         self.assertEqual((0 * b).value, 0)
         self.assertEqual((3 * b).value, 6)
         self.assertEqual((8 * b).value, 7)
+
+    def test_sub(self):
+        a = Integer(7)
+        b = Integer(2)
+        self.assertEqual((a - b).value, 5)
+        self.assertEqual((a - 0).value, a.value)
+        self.assertEqual((a - 1).value, 6)
+        self.assertEqual((a - 8).value, 8)
+        self.assertEqual((0 - b).value, 7)
+        self.assertEqual((3 - b).value, 1)
+        self.assertEqual((8 - b).value, 6)
+
+    def test_neg(self):
+        a = Integer(7)
+        self.assertEqual((-a).value, 2)
 
 if __name__ == '__main__':
     unittest.main()
