3ea49542e1a6e66669726446b7d4d4dc4d67a952
Belousow Makc <lib.bmw@gmail.com>
2014-10-15 21:42:44 +0400

    Add tests for multiplication

--- tests.py
+++ tests.py
@@ -18,6 +18,16 @@ class IntegerTest(unittest.TestCase):
         self.assertEqual((3 + b).value, 5)
         self.assertEqual((8 + b).value, 1)
 
+    def test_multiplication(self):
+        a = Integer(7)
+        b = Integer(2)
+        self.assertEqual((a * b).value, 5)
+        self.assertEqual((a * 1).value, a.value)
+        self.assertEqual((a * 2).value, 5)
+        self.assertEqual((a * 0).value, 0)
+        self.assertEqual((0 * b).value, 0)
+        self.assertEqual((3 * b).value, 6)
+        self.assertEqual((8 * b).value, 7)
 
 if __name__ == '__main__':
     unittest.main()
